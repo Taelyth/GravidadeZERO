d9ad93c4de2150cdd21cfd5ef97367810339141b
Taelyth <jaquebeneduzzi@hotmail.com>
2022-04-11 16:09:25 -0300

    Testando Secrets do Git Hub - part 3

--- getgeeks/project/frontend/resources/factories/Database.py
+++ getgeeks/project/frontend/resources/factories/Database.py
@@ -1,12 +1,23 @@
-import config
+import os
 
 
 def factory_database():
-    database = {
-        'banco': config.BANCO,
-        'usuario': config.USUARIO,
-        'senha': config.SENHA,
-        'server': config.SERVER,
-        'porta': config.PORTA
-    }
+    try:  # This should run locally
+        import config
+
+        database = {
+            'banco': config.BANCO,
+            'usuario': config.USUARIO,
+            'senha': config.SENHA,
+            'server': config.SERVER,
+            'porta': config.PORTA
+        }
+    except:  # This should run on GitHub Actions
+        database = {
+            'banco': os.environ['BANCO'],
+            'usuario': os.environ['USUARIO'],
+            'senha': os.environ['SENHA'],
+            'server': os.environ['SERVER'],
+            'porta': os.environ['PORTA']
+        }
     return database
